944edd13e5cb8ab7545110e0cf918ecccd76a23c
Giancarlo Joel Alvarez Gallardo <161910868+GiancarloDev21@users.noreply.github.com>
2024-04-17 23:26:32 -0500

    Subida conexion on login front

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const { swaggerUi, specs } = require('./config/swagger');
 const authRoutes = require('./api/routes/authRoutes');
 
 const app = express();
-
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:3680', // Origen permitido
+    optionsSuccessStatus: 200, // Algunos navegadores requieren esta opción
+  }));
 // Connect to MongoDB
 connectDB();
 
 // Init Middleware
 app.use(express.json());
+//Uso del cors para permitir que el frontend se conecte correctamente
 
 // Define Routes
 app.use('/api/auth', authRoutes);
@@ -19,4 +24,8 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 3000;
 
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Habilita cors con opciones específicas
+
+  
