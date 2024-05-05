a0db583754d43eefc3b86d701de879fc8a16ea69
Giancarlo Joel Alvarez Gallardo <161910868+GiancarloDev21@users.noreply.github.com>
2024-05-05 17:52:03 -0500

    Subida de cadena de conexion en la nube de  mongoDB atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/swgipridatabase', {
+    await mongoose.connect('mongodb+srv://201110624:6565@risacmp-cluster.nsjbeyz.mongodb.net/risacmp_db_desarrollo?retryWrites=true&w=majority&appName=risacmp-cluster', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
