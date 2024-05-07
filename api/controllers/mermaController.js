c3763b9b0f782a248ee6d0d7dbcec55692ff49c8
Christian Alvarez <116127459+Chablas@users.noreply.github.com>
2024-05-07 17:46:56 -0500

    añadido controladores

---
+++ api/controllers/mermaController.js
@@ -0,0 +1,58 @@
+const Merma = require('../models/MermaModel');
+
+const obtenerTodosLasMermas = async (req, res) => {
+    try {
+        const merma = await Merma.find({});
+        res.status(200).json(merma);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const obtenerMermaPorId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const merma = await Merma.findById(id);
+        res.status(200).json(merma);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const crearMerma = async (req, res) => {
+    try {
+        const merma = await Merma.create(req.body);
+        res.status(200).json(merma);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const actualizarMerma = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const merma = await Merma.findByIdAndUpdate(id, req.body);
+        if (merma) {
+            return res.status(404).json({ message: "Merma no encontrada" });
+        }
+        const mermaActualizada = await Merma.findById(id);
+        res.status(200).json(mermaActualizada);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+const eliminarMerma = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const merma = await Merma.findByIdAndDelete(id);
+        if (merma) {
+            return res.status(404).json({ message: "Merma no encontrada" });
+        }
+        res.status(200).json({ message: "Merma eliminada exitosamente" });
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+module.exports = { obtenerTodosLasMermas, obtenerMermaPorId, crearMerma, actualizarMerma, eliminarMerma }
--- api/routes/mermaRoutes.js
+++ api/routes/mermaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Merma = require('../models/MermaModel');
+const mermaController = require('../controllers/mermaController');
 
 /**
  * @swagger
@@ -16,14 +16,7 @@ const Merma = require('../models/MermaModel');
  *       500:
  *         description: Server error
  */
-router.get('/get/all', async (req, res) => {
-    try {
-        const merma = await Merma.find({});
-        res.status(200).json(merma);
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+router.get('/get/all', mermaController.obtenerTodosLasMermas);
 
 /**
  * @swagger
@@ -31,6 +24,13 @@ router.get('/get/all', async (req, res) => {
  *   get:
  *     summary: Obtener Merma por su id
  *     tags: [Merma]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la merma a buscar
+ *         schema:
+ *           type: string
  *     responses:
  *       200:
  *         description: Datos obtenidos satisfactoriamente
@@ -39,15 +39,7 @@ router.get('/get/all', async (req, res) => {
  *       500:
  *         description: Server error
  */
-router.get('/get/:id', async (req, res) => {
-    try {
-        const {id}=req.params;
-        const merma = await Merma.findById(id);
-        res.status(200).json(merma);
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+router.get('/get/:id', mermaController.obtenerMermaPorId);
 
 /**
  * @swagger
@@ -55,6 +47,23 @@ router.get('/get/:id', async (req, res) => {
  *   post:
  *     summary: Registrar una merma
  *     tags: [Merma]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               cantidad:
+ *                 type: number
+ *               fecha_conteo:
+ *                 type: date
+ *               responsable_encargado:
+ *                 type: string
+ *               motivo:
+ *                 type: string
  *     responses:
  *       200:
  *         description: Datos creados satisfactoriamente
@@ -63,14 +72,7 @@ router.get('/get/:id', async (req, res) => {
  *       500:
  *         description: Server error
  */
-router.post('/post', async (req, res) => {
-    try {
-        const merma = await Merma.create(req.body);
-        res.status(200).json(merma);
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+router.post('/post', mermaController.crearMerma);
 
 /**
  * @swagger
@@ -78,6 +80,30 @@ router.post('/post', async (req, res) => {
  *   put:
  *     summary: Actualizar una merma
  *     tags: [Merma]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la merma a actualizar
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               cantidad:
+ *                 type: number
+ *               fecha_conteo:
+ *                 type: date
+ *               responsable_encargado:
+ *                 type: string
+ *               motivo:
+ *                 type: string
  *     responses:
  *       200:
  *         description: Datos actualizados satisfactoriamente
@@ -86,19 +112,7 @@ router.post('/post', async (req, res) => {
  *       500:
  *         description: Server error
  */
-router.put('/put/:id', async (req, res) => {
-    try {
-        const {id}=req.params;
-        const merma = await Merma.findByIdAndUpdate(id, req.body);
-        if(!merma){
-            return res.status(404).json({message: "Merma no encontrada"});
-        }
-        const mermaActualizada = await Merma.findById(id);
-        res.status(200).json(mermaActualizada);
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+router.put('/put/:id', mermaController.actualizarMerma);
 
 /**
  * @swagger
@@ -106,6 +120,13 @@ router.put('/put/:id', async (req, res) => {
  *   delete:
  *     summary: Eliminar una merma
  *     tags: [Merma]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de la merma a eliminar
+ *         schema:
+ *           type: string
  *     responses:
  *       200:
  *         description: Datos eliminados satisfactoriamente
@@ -114,17 +135,6 @@ router.put('/put/:id', async (req, res) => {
  *       500:
  *         description: Server error
  */
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const {id}=req.params;
-        const merma = await Merma.findByIdAndDelete(id);
-        if(!merma){
-            return res.status(404).json({message: "Merma no encontrada"});
-        }
-        res.status(200).json({message: "Merma eliminada exitosamente"});
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-});
+router.delete('/delete/:id', mermaController.eliminarMerma);
 
 module.exports = router;
